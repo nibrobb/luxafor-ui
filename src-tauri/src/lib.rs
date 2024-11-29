3e85ff6ef588e2821e697b29f35f65b1441192fb
Robin Kristiansen <1232179+nibrobb@users.noreply.github.com>
2024-11-29 14:37:07 +0100

    Working luxafor backend

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,32 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use luxafor::usb_hid::USBDeviceDiscovery;
+use luxafor::Device;
+use luxafor::SolidColor;
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn set_light_color(color: &str) -> Result<(), String> {
+    let discovery = USBDeviceDiscovery::new().map_err(|e| e.to_string())?;
+    let device = discovery.device().map_err(|e| e.to_string())?;
+
+    println!("USB Device: {:?}", device.id());
+
+    let parsed_color = match color {
+        "green" => SolidColor::Green,
+        "red" => SolidColor::Red,
+        "blue" => SolidColor::Blue,
+        _ => return Err("Unsupported color".to_string()),
+    };
+
+    device
+        .set_solid_color(parsed_color)
+        .map_err(|e| e.to_string())
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![set_light_color])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/app.rs
+++ src/app.rs
@@ -10,60 +10,53 @@ extern "C" {
 }
 
 #[derive(Serialize, Deserialize)]
-struct GreetArgs<'a> {
-    name: &'a str,
+struct ColorArgs<'a> {
+    color: &'a str,
 }
 
 #[component]
 pub fn App() -> impl IntoView {
-    let (name, set_name) = create_signal(String::new());
-    let (greet_msg, set_greet_msg) = create_signal(String::new());
+    let (color, set_color) = create_signal(String::new());
 
-    let update_name = move |ev| {
+    let update_color = move |ev| {
         let v = event_target_value(&ev);
-        set_name.set(v);
+        set_color.set(v);
     };
-
-    let greet = move |ev: SubmitEvent| {
+    let on_set_color = move |ev: SubmitEvent| {
         ev.prevent_default();
         spawn_local(async move {
-            let name = name.get_untracked();
-            if name.is_empty() {
+            let color = color.get_untracked();
+            if color.is_empty() {
                 return;
             }
 
-            let args = serde_wasm_bindgen::to_value(&GreetArgs { name: &name }).unwrap();
+            let args = serde_wasm_bindgen::to_value(&ColorArgs { color: &color }).unwrap();
             // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-            let new_msg = invoke("greet", args).await.as_string().unwrap();
-            set_greet_msg.set(new_msg);
+            let color = invoke("set_light_color", args).await.as_string().unwrap();
+            set_color.set(color);
         });
     };
-
     view! {
-        <main class="container">
-            <h1>"Welcome to Tauri + Leptos"</h1>
-
+        <div class="header">
             <div class="row">
                 <a href="https://tauri.app" target="_blank">
-                    <img src="public/tauri.svg" class="logo tauri" alt="Tauri logo"/>
+                    <img width="75" src="public/tauri.svg" class="logo tauri" alt="Tauri logo"/>
                 </a>
                 <a href="https://docs.rs/leptos/" target="_blank">
-                    <img src="public/leptos.svg" class="logo leptos" alt="Leptos logo"/>
+                    <img width="75" src="public/leptos.svg" class="logo leptos" alt="Leptos logo"/>
                 </a>
             </div>
-            <p>"Click on the Tauri and Leptos logos to learn more."</p>
-            <form class="row" on:submit=greet>
+        </div>
+        <main class="container">
+            <form class="row" on:submit=on_set_color>
                 <input
-                    id="greet-input"
+                    id="luxafor-color"
                     placeholder="Enter a name..."
-                    on:input=update_name
+                    on:input=update_color
                 />
                 <button type="submit">"Greet"</button>
             </form>
-            <p>{ move || greet_msg.get() }</p>
+            <p>{ move || color.get() }</p>
         </main>
-        <div>
-            <p>How to center a div</p>
-        </div>
     }
 }
