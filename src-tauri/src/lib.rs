b6514b3f1a19af522251cde430970125201c2639
Robin Kristiansen <1232179+nibrobb@users.noreply.github.com>
2025-02-21 01:14:58 +0100

    Hide window upon closing instad of exiting
    * Add support for letting the window run as a "background task"
    * Minor tweaks
    * `cargo update`

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,5 @@
 use std::str::FromStr;
+use tauri::tray::{MouseButton, MouseButtonState, TrayIconEvent};
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 use luxafor::{usb_hid::USBDeviceDiscovery, Device, SolidColor};
@@ -6,6 +7,7 @@ use luxafor::{usb_hid::USBDeviceDiscovery, Device, SolidColor};
 use tauri::{
     menu::{AboutMetadataBuilder, Menu, MenuItem, PredefinedMenuItem},
     tray::TrayIconBuilder,
+    Manager, WindowEvent,
 };
 
 #[tauri::command]
@@ -33,7 +35,7 @@ fn set_light_color(color: &str) -> Result<(), String> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .setup(|app| {
+        .setup(move |app| {
             let aboutmeta = AboutMetadataBuilder::new()
                 .authors(Some(vec![String::from("Robin Kristiansen")]))
                 .icon(Some(app.default_window_icon().unwrap().clone()))
@@ -46,18 +48,41 @@ pub fn run() {
             )?;
             let _tray = TrayIconBuilder::new()
                 .menu(&menu)
+                .tooltip("Luxafor-ui")
                 .show_menu_on_left_click(true)
                 .icon(app.default_window_icon().unwrap().clone())
                 .on_menu_event(|app, event| match event.id.as_ref() {
                     "quit" => {
                         app.exit(0);
                     }
-                    _ => {
-                        eprintln!("Menu item {:?} not handled", event.id);
+                    _ => {}
+                })
+                .on_tray_icon_event(|tray, event| match event {
+                    TrayIconEvent::Click {
+                        button: MouseButton::Left,
+                        button_state: MouseButtonState::Up,
+                        ..
+                    } => {
+                        if let Some(window) = tray.app_handle().get_webview_window("main") {
+                            window.show().unwrap();
+                            window.set_focus().unwrap();
+                        }
                     }
+                    _ => {}
                 })
                 .build(app)?;
             Ok(())
+        })
+        .on_window_event(|window, event| {
+            if let Some(main_window) = window.app_handle().get_webview_window("main") {
+                match event {
+                    WindowEvent::CloseRequested { api, .. } => {
+                        api.prevent_close();
+                        main_window.hide().unwrap();
+                    }
+                    _ => {}
+                }
+            }
         })
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![set_light_color])
