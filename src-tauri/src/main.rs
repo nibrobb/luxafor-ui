0c679ef1fcbe00ae8be05f81b0cd71a84278f494
Robin Kristiansen <1232179+nibrobb@users.noreply.github.com>
2025-11-23 02:55:45 +0100

    Refactor more Slack API related stuff to its own module
    * Add feature 'tracing' and utilize feature 'slack_sync' more
    * Hide shit begind cfg( ... ) attributes
    * Update Leptos

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,14 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-mod slack_api;
-
 fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let subscriber = tracing_subscriber::fmt::Subscriber::builder()
-        .with_max_level(tracing::Level::DEBUG)
-        .finish();
-    tracing::subscriber::set_global_default(subscriber)?;
+    #[cfg(feature = "tracing")]
+    {
+        let subscriber = tracing_subscriber::fmt::Subscriber::builder()
+            .with_max_level(tracing::Level::DEBUG)
+            .finish();
+        tracing::subscriber::set_global_default(subscriber)?;
+    }
 
     luxafor_ui_lib::run()
 }
--- src-tauri/src/slack_api.rs
+++ src-tauri/src/slack_api.rs
@@ -1,14 +1,16 @@
+use serde::{Deserialize, Serialize};
 use slack_morphism::prelude::*;
-use tracing::{debug, error};
 
-#[allow(unused)]
-pub(crate) async fn status_set(
+#[cfg_attr(feature = "tracing", tracing::instrument(skip_all))]
+pub async fn status_set(
     profile: SlackUserProfile,
     user_token: SlackApiTokenValue,
 ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    debug!(
+    #[cfg(feature = "tracing")]
+    tracing::debug!(
         "status_text: {:?}, status_emoji: {:?}",
-        profile.status_text, profile.status_emoji
+        profile.status_text,
+        profile.status_emoji
     );
 
     let client = SlackClient::new(SlackClientHyperConnector::new()?);
@@ -26,8 +28,75 @@ pub(crate) async fn status_set(
     {
         Ok(_something) => Ok(()),
         Err(e) => {
-            error!("Error setting status: {:#?}", e);
+            #[cfg(feature = "tracing")]
+            tracing::error!("Error setting status: {:#?}", e);
             Err(Box::new(e))
         }
+    }
+}
+
+#[cfg_attr(
+    feature = "tracing",
+    tracing::instrument(
+        skip_all,
+        fields(status_text = ?profile.status_text, emoji = ?profile.status_emoji)
+    )
+)]
+pub async fn slack_set_profile(
+    profile: SlackUserProfile,
+    tokens: SlackApiTokens,
+) -> Result<(), String> {
+    if let Some(token) = tokens.user_token {
+        if !token.0.starts_with("xoxp-") {
+            #[cfg(feature = "tracing")]
+            tracing::error!("user_token is not a valid user token: {:?}", token);
+            return Err("user_token is not a valid user token".to_string());
+        }
+        if let Err(e) =
+            tauri::async_runtime::spawn(
+                async move { status_set(profile.clone(), token.clone()).await },
+            )
+            .await
+        {
+            Err(e.to_string())
+        } else {
+            Ok(())
+        }
+    } else {
+        Err("user_token not found".to_string())
+    }
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Default)]
+pub struct SlackApiTokens {
+    user_token: Option<SlackApiTokenValue>,
+    bot_token: Option<SlackApiTokenValue>,
+}
+
+impl SlackApiTokens {
+    pub fn new(
+        user_token: Option<SlackApiTokenValue>,
+        bot_token: Option<SlackApiTokenValue>,
+    ) -> Self {
+        SlackApiTokens {
+            user_token,
+            bot_token,
+        }
+    }
+}
+
+impl TryFrom<serde_json::Value> for SlackApiTokens {
+    type Error = serde_json::Error;
+
+    fn try_from(value: serde_json::Value) -> Result<Self, Self::Error> {
+        let user_token = value
+            .get("user_token")
+            .and_then(|v| v.as_str())
+            .map(|s| s.into());
+        let bot_token = value
+            .get("bot_token")
+            .and_then(|v| v.as_str())
+            .map(|s| s.into());
+        Ok(SlackApiTokens::new(user_token, bot_token))
     }
 }
