4f3496d6b79f2e679e6ba918e0c36938cfb603a4
Robin Kristiansen <1232179+nibrobb@users.noreply.github.com>
2026-02-19 23:08:06 +0100

    .

--- debian/update.sh
+++ debian/update.sh
@@ -2,20 +2,36 @@
 
 set -x
 
-# TODO: make this more dynamic
-luxafor_ui_deb='Luxafor-ui_0.1.0_amd64.deb'
+# sudo apt update
+# sudo apt install -y wget git gpg apt-utils dpkg-dev
+# git checkout v${{ github.event.inputs.version }}
+# cd ${{ github.workspace }}
+# gh release download debian -D debian -R nibrobb/luxafor-ui
+# mv -v ./*.deb ./debian # hmm
+# cd debian
 
 # Get the OS code-name ("noble" on Ubuntu 24.04 LTS)
-. /etc/os-release
-codename="$VERSION_CODENAME"
+codename="$(lsb_release -sc 2>/dev/null)"
 # TODO: Make this also more dynamic
 deb_arch='amd64'
 
-mkdir -p "dists/$codename/main/binary-amd64"
+mkdir -p "dists/$codename/main/binary-$deb_arch"
 mkdir -p pool/main/luxafor-ui/
 
-cp Distributions "dists/$codename" || exit 1
-cp "../target/release/bundle/deb/$luxafor_ui_deb" "pool/main/$luxafor_ui_deb" || exit 1
+sed -i "s/%codename%/$codename/g" Distributions
+sed -i "s/%arch%/$deb_arch/g" Distributions
+cat <<EOF > Distributions
+Origin: luxafor-ui
+Label: nibrobb luxafor-ui
+Suite: $codename
+Codename: $codename
+Architectures: $deb_arch
+Components: main
+Description: Graphical desktop application for controlling a Luxafor FLAG­™
+EOF
+
+cp Distributions "dists/$codename/" || exit 1
+cp "../target/release/bundle/deb/*.deb" pool/main/luxafor-ui/ || exit 1
 
 apt-ftparchive --arch $deb_arch packages pool/ > "dists/$codename/main/binary-$deb_arch/Packages"
 gzip -kf "dists/$codename/main/binary-$deb_arch/Packages"
@@ -23,5 +39,8 @@ gzip -kf "dists/$codename/main/binary-$deb_arch/Packages"
 cd "dists/$codename"
 cat Distributions > Release
 apt-ftparchive release . >> Release
-gpg --clearsign --yes -o InRelease Release
-gpg -abs --yes -o Release.gpg Release
+gpg --default-key "${GPG_SIGNING_EMAIL}" --clearsign --yes -o InRelease Release
+gpg --default-key "${GPG_SIGNING_EMAIL}" --armor --detach-sign --sign --yes -o Release.gpg Release
+
+
+# gh release upload debian ./* --clobber -R nibrobb/luxafor-ui
